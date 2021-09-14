d3145cae3e2b90830fccce25381dbad25bf16878
DucTran2310 <18520021@gm.uit.edu.vn>
2021-09-14 11:10:46 +0700

    Update export

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export {default as Navbar} from './Navbar/Navbar';
 export {default as InforSection} from './InforSection/InforSection';
+export {default as Pricing} from './Pricing/Pricing';
+export {default as Footer} from './Footer/Footer';
